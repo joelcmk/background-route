936e97be502ed9a49550a6059f1430623a5b57f4
joelcmk <57118300+joelcmk@users.noreply.github.com>
2020-08-10 12:15:36 -0500

    Created components and added state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { Index, Users, About } from './components'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { color: '#282c34' }
+  }
+
+  changeColor = color => {
+    this.setState({ color })
+  }
+
+  render() {
+    return (
+      <div style={{ background: this.state.color }} id="main">
+        <Router>
+          <div>
+            <nav>
+              <ul>
+                <li>
+                  <Link
+                    to="/"
+                    onClick={() => this.changeColor("#282c34")}
+                  >
+                    Home
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    onClick={() => this.changeColor("#bc27b0")}
+                    to="/about"
+                  >
+                    About
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/users" onClick={() => this.changeColor('#007bff')} >Users</Link>
+                </li>
+              </ul>
+            </nav>
+
+            <Route path="/" exact component={Index} />
+            <Route path="/about" component={About} />
+            <Route path="/users" component={Users} />
+          </div>
+        </Router>
+      </div >
+    );
+  }
+
 }
 
 export default App;
---
+++ src/components.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+export const Index = () => {
+  return (
+    <>
+      <h2>Home</h2>
+      <p>This is the Home Page</p>
+    </>
+  );
+}
+
+export const About = () => {
+  return (
+    <>
+      <h2>About</h2>
+      <p>This is the About Page</p>
+    </>
+  );
+}
+
+export const Users = () => {
+  return (
+    <>
+      <h2>Users</h2>
+      <p>This is the Users Page</p>
+    </>
+  )
+}
